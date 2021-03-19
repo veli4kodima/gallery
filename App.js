54423a83af55926f6adf15f23ceba1bccc551e18
veli4kodima <veli4kodima@gmail.com>
2021-03-19 08:46:54 +0200

    gallery first commit

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet, Text, View, ScrollView } from 'react-native';
+import ImageCard from './ImageCard';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const url = 'https://jsonplaceholder.typicode.com/photos?_page=1&_limit=20'
+export default class App extends Component {
+  state = {
+    gallery: []
+  }
+
+  componentDidMount = async() => {
+    const response = await fetch(url);
+    const request = await response.json();
+    this.setState({ gallery: request });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+          { this.state.gallery.map(item => {
+            console.log('item', item);
+            <ImageCard data={item} key={item.id}/>
+          })
+          }
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    width: 300,
+    height: 500,
+  }
 });
